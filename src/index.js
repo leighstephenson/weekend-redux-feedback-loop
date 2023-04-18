0a4269d7ad3b51e815d253d29f3349292d5d8330
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-04-17 21:50:38 -0500

    finished making base mode  reducers in index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,68 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 //feeling - understanding - support - comments > review
 
-const feeling= (state = '', action) => {
+const feeling = (state = '0', action) => {
     if (action.type === 'SET_FEELING') {
         //changes the value of reducer
         return action.payload;
-    }
-    return state;
-}
+    } else if (action.type === 'CLEAR_FORM') {
+        return 0
+    };
+    return state
+};
+
+const understanding = (state = 0, action) => {
+    if (action.type === 'SET_UNDERSTANDING') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_FORM') {
+        return 0
+    };
+    return state
+};
+
+const support = (state = 0, action) => {
+    if (action.type === 'SET_SUPPORT') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_FORM') {
+        return 0
+    };
+    return state
+};
+
+const comments = (state = '', action) => {
+    if (action.type === 'SET_COMMENTS') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_FORM') {
+        return ''
+    };
+    return state
+};
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            feeling,
+            understanding,
+            support,
+            comments,
+            //TODO Add reducers here
+        }
+    ),
+    applyMiddleware(logger)
+
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
