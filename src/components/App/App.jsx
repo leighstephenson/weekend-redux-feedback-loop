66c900c3aabe8f6eaea9da715df3f05deb5d37a9
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-04-17 22:22:12 -0500

    added Router and routes to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-//TODO more imports
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
 
+  //! What will display on the DOM
   return (
     <div className='App'>
+      <Router>
+
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      
+      <Route exact path="/">
+          <Feeling />
+        </Route>
+
+        <Route exact path="/understanding">
+          <Understanding />
+        </Route>
+
+        <Route exact path="/support">
+          <Support />
+        </Route>
+
+        <Route exact path="/comments">
+          <Comments />
+        </Route>
+
+        <Route exact path="/review">
+          <Review />
+        </Route>
+
+      </Router>
     </div>
   );
 }
