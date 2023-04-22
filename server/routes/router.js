c0d5dfcdbb07aa116e1ea2bf65c34e5fabc39ad5
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-04-22 18:35:14 -0500

    Added get and post requests on router.js

---
+++ server/routes/router.js
@@ -0,0 +1,32 @@
+const express = require = ('express');
+const pool = require('');
+const { query } = require('express');
+const router = express.Router();
+
+
+//!POST request
+router.post('/', (req, res) => {
+    console.log('Testing in POST on router.js');
+    let queryText = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);';
+    let { feeling, understanding, support, comments } = req.body;
+    pool.query(queryText, [feeling, understanding, support, comments]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500)
+    });
+})
+
+//! GET request
+router.get('/', (req, res) => {
+    console.log('Testing GET in router');
+    let queryText = 'SELECT * from "feedback";';
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error in GET on router.js: ${error}`);
+    })
+});
+
+
+module.exports = router;
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,12 +1,54 @@
 //TODO imports here
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Card, Typography } from '@mui/material';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 function Review() {
+    const history = useHistory()
+    const feelingRating = useSelector(store => store.feeling);
+    const understandingRating = useSelector(store => store.understanding);
+    const supportRating = useSelector(store => store.support);
+    const additionalComments = useSelector(store => store.comments);
 
 
+//TODO need to build the success page and route on App
+    //! Submit feedback
+    const submit = () => {
+        axios.post('/feedback', {
+            feeling: feelingRating,
+            understanding: understandingRating,
+            support: supportRating,
+            comments: additionalComments,
+        }).then(response => {
+            history.push('/success');
+        }).catch(error => {
+            alert('Something is wrong in POST on Review');
+            console.log(error)
+        });
+    };
+
+    //! What displays 
+    //feeling > understanding > support > comments > review
     return (
 
         <>
+            <ProgressBar currentStep={5} />
+            <Typography>
+                Review your answers.
+            </Typography>
 
+            <Card>
+                <form onSubmit={submit}>
+                    <p>Feeling: {feelingRating} </p>
+                    <p>Understanding: {understandingRating} </p>
+                    <p> Support: {supportRating} </p>
+                    <p> Comments: {additionalComments} </p>
+                    <input type="submit" value="SUBMIT" />
+
+                </form>
+            </Card>
         </>
     )
 } // end Review ()
